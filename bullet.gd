36a908d10f905d42385158c50e355d192f1e6a6c
xturnalgamer <77756980+william-can@users.noreply.github.com>
2023-12-05 11:14:48 -0600

    beginning code for bullets

--- bullet.gd
+++ bullet.gd
@@ -1,11 +1,29 @@
 extends Node2D
 
+@export
+#makes bullet go fast
+var bullet_speed:int = 750
+@export
+#makes that guy go ow
+var bullet_damage:int = 1
+@export 
+#prevents loss of damage
+var bullet_pierce:int = 0
+@export
+#set bullet's collison -1: bad 0: neutrial (dmg all) 1: good
+var bullet_team:int = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _physics_process(delta):
+	position += transform.x * bullet_speed * delta
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("mobs"):
+		
+		body.damage(bullet_damage)
+		if bullet_pierce > 0:
+			bullet_pierce -= 1
+		bullet_damage -= body.health
+		
+	if body.is_in_group("walls"):
+		queue_free()
